59c4c8c28a1fc961e4e4aaf046ded2225bbcfbb9
Joe DiVita <joediv31@gmail.com>
2016-02-14 15:31:13 -0600

    remove .js files from src

--- src/app/js/config/routes.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-var React = require('react');
-var MainLayout_1 = require('../components/MainLayout/MainLayout');
-var react_router_1 = require('react-router');
-var routes = (React.createElement(react_router_1.Route, {path: "/", component: MainLayout_1.MainLayout}));
-exports.routes = routes;
-//# sourceMappingURL=routes.js.map
--- src/app/js/main.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-require('../styles/app.sass');
-var React = require('react');
-var ReactDOM = require('react-dom');
-var react_router_1 = require('react-router');
-var react_router_2 = require('react-router');
-var routes_1 = require('./config/routes');
-ReactDOM.render(React.createElement(react_router_1.Router, {history: react_router_2.hashHistory}, routes_1.routes), document.getElementById('main'));
-//# sourceMappingURL=main.js.map
