b742fbfdd4d0c5e17cfb28ab56a9c3feaf5cd625
Joe DiVita <joediv31@gmail.com>
2016-02-15 14:30:37 -0600

    remove js test files

--- src/test/hello.spec.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-var chai_1 = require('chai');
-describe('hello', function () {
-    it('should work', function () {
-        chai_1.expect(true).to.eq(true);
-    });
-});
-//# sourceMappingURL=hello.spec.js.map
