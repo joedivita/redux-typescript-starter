cda35d07defa04496c3a43fb75fcd13dd44ebf55
Joe DiVita <joediv31@gmail.com>
2016-02-17 11:06:14 -0600

    adding initial store, state, & reducer to main file

--- src/app/js/main.tsx
+++ src/app/js/main.tsx
@@ -10,6 +10,12 @@ import { Router } from 'react-router';
 import { hashHistory } from 'react-router';
 import { routes } from './config/routes';
 
+import { rootReducer } from './modules/reducers';
+import { Store, createStore } from 'redux';
+
+const initialState = {};
+const store: Store = createStore(rootReducer, initialState);
+
 ReactDOM.render(
   <Router history={hashHistory}>{routes}</Router>,
   document.getElementById('main')
