98df188bae4e309c46551591db5cde5854e1faf6
Joe DiVita <joediv31@gmail.com>
2016-02-17 18:15:09 -0600

    clear test for now

--- src/test/components/MainLayout/MainLayout.spec.tsx
+++ src/test/components/MainLayout/MainLayout.spec.tsx
@@ -1,10 +1,9 @@
 import * as React from 'react';
 import * as ReactTestUtils from 'react-addons-test-utils';
 import MainLayout from '../../../app/js/components/MainLayout/MainLayout';
-import { Dispatch } from 'redux';
 
 describe('Footer', () => {
   it('renders', () => {
-
+    
   });
 });
