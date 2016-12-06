e11859841f4a65686530e25f974d19fcb2fec9e3
Joe DiVita <joediv31@gmail.com>
2016-12-05 18:54:48 -0600

    adding redux-async-connect typings

---
+++ typings/redux-async-connect/redux-async-connect.d.ts
@@ -0,0 +1,16 @@
+/// <reference path="../redux/redux.d.ts" />
+/// <reference path="../react-router/history.d.ts" />
+
+declare module 'redux-async-connect' {
+  export function asyncConnect(state: Object): ClassDecorator;
+  export var reducer: any;
+  export var ReduxAsyncConnect: any;
+  export interface ReduxAsyncConnectState {
+    loaded: boolean;
+  }
+  export interface PromiseOptions {
+    store: Redux.Store;
+    location: HistoryModule.Location;
+    params: any;
+  }
+}
